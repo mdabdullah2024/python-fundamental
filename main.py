06ae7ff7eb938a1cdffbefe468862b35500b79b4
Md. Abdullah <mdabd1992@gmail.com>
2024-05-11 16:35:39 +0600

    Authentication&Authorization

--- main.py
+++ main.py
@@ -10,11 +10,59 @@ from flask_principal import Principal,Permission,RoleNeed
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///booking.db'
-app.config('SECRET_KEY') = 'secret_key'
+app.config['SECRET_KEY'] = 'secret_key'
 db = SQLAlchemy(app)
 
 loginManager = LoginManager(app)
 principal = Principal(app) 
+
+class User(UserMixin,db.Model):
+    id = db.Column(db.Integer,primary_key= True)
+    username = db.Column(db.String(50),unique= True,nullable=False)
+    password = db.Column(db.String(50),nullable= False)
+
+@loginManager.user_loader
+
+def loaduser(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route('/login',methods=['GET','POST'])
+
+def login():
+    if request.method  == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username = username,password = password).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            return "Invalid Username or Password!"
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method  == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        existuser = User.query.filter_by(username = username,password = password).first()
+        if existuser:
+            return "Username already exists"
+        new_user = User(username = username,password = password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+
+
 
 class Booking(db.Model):
     id = db.Column(db.Integer, primary_key = True)
